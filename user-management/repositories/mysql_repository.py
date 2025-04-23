04aa642ee621fffeb70ca4b19d13639784de420e
Alper Timucin Akyol <akyolalper89@gmail.com>
2025-04-23 22:43:15 +0300

    baglantılar classlar ile sağlandı

---
+++ user-management/repositories/mysql_repository.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+from flask_mysqldb import MySQL
+
+class BaseRepository(ABC):
+    @abstractmethod
+    def save(self, data):
+        pass
+
+    @abstractmethod
+    def find_by_username(self, username):
+        pass
+
+class MySQLUserRepository(BaseRepository):
+    def __init__(self, mysql: MySQL):
+        self.mysql = mysql
+
+    def save(self, data):
+        try:
+            cursor = self.mysql.connection.cursor()
+            cursor.execute("USE users")
+            cursor.execute(
+                "INSERT INTO user (username, password, email, user_type) VALUES (%s, %s, %s, %s)",
+                (data['username'], data['password'], data['email'], data['user_type'])
+            )
+            self.mysql.connection.commit()
+            cursor.close()
+            return True
+        except Exception as e:
+            print(f"Kayıt hatası: {str(e)}")
+            return False
+
+    def find_by_username(self, username):
+        try:
+            cursor = self.mysql.connection.cursor()
+            cursor.execute("USE users")
+            cursor.execute(
+                "SELECT user_id, username, password, email, user_type FROM user WHERE username = %s",
+                (username,)
+            )
+            user = cursor.fetchone()
+            cursor.close()
+            if user:
+                return {
+                    'user_id': user[0],
+                    'username': user[1],
+                    'password': user[2],
+                    'email': user[3],
+                    'user_type': user[4],
+                }
+            return None
+        except Exception as e:
+            print(f"Arama hatası: {str(e)}")
+            return None
